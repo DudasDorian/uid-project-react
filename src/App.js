ce71fd4a706d6c52ecce8795a28027cf090a17ca
Chindea Tudor Eugen <tudor_chindea@yahoo.com>
2023-01-17 23:04:27 +0200

    local guides page with chat

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import RestaurantReservation from "./components/RestaurantReservation";
 import RestaurantReview from "./components/RestaurantReview";
 import NearbyAttractions from "./components/NearbyAttractions";
 import NavBar from "./components/NavBar";
+import LocalGuides from "./components/LocalGuides";
 
 function App() {
   const { username, saveLogin } = useStorage()
@@ -45,6 +46,7 @@ function App() {
           <Route path="/restaurants" component={RestaurantList} />
           <Route path="/reserve-restaurant" component={RestaurantReservation} />
           <Route path="/review-restaurant" component={RestaurantReview} />
+          <Route path="/local-guides" component={LocalGuides} />
           <Route path="/user-details/:userId" >
               <UserDetails userId={1}/>
           </Route>
